d73baf0e6b27ec09fe4242ccdedd4ff544e79232
ryhkml <reyhank95@hotmail.com>
2024-12-10 11:58:52 +0700

    Update to Angular 18 with experimental zoneless

--- server.ts
+++ server.ts
@@ -1,5 +1,3 @@
-import "zone.js/node";
-
 import { APP_BASE_HREF } from "@angular/common";
 import { CommonEngine } from "@angular/ssr";
 
@@ -17,11 +15,19 @@ import { mw } from "request-ip";
 import * as express from "express";
 import * as compression from "compression";
 import * as cookieParser from "cookie-parser";
-import * as csurf from "csurf";
 
-import { getAllDatasetController, addDatasetController, downloadDatasetController, removeDatasetController, getInstructionStateController, saveInstructionStateController, removeInstructionStateController, getDatasetController, replaceDatasetController } from "server/controller/jsonl.controller";
+import {
+	getAllDatasetController,
+	addDatasetController,
+	downloadDatasetController,
+	removeDatasetController,
+	getInstructionStateController,
+	saveInstructionStateController,
+	removeInstructionStateController,
+	getDatasetController,
+	replaceDatasetController
+} from "server/controller/jsonl.controller";
 import { imageOCRController } from "server/controller/document-ocr.controller";
-import { exceptionToken } from "server/middlewares/exception-token.middleware";
 import { imageOCRUpload } from "server/middlewares/multer.middleware";
 import { headerApi } from "server/middlewares/header-api.middleware";
 import { logInfo } from "server/services/logger.service";
@@ -33,144 +39,133 @@ import bootstrap from "./src/main.server";
 export const env = new BehaviorSubject<EnvVar>({});
 
 export function app() {
-    const server = express();
-    const browserDir = join(cwd(), "dist/fine-tune-forge/browser");
-    const indexHtml = existsSync(join(browserDir, "index.original.html"))
-        ? join(browserDir, "index.original.html")
-        : join(browserDir, "index.html");
+	const server = express();
+	const browserDir = join(cwd(), "dist/fine-tune-forge/browser");
+	const indexHtml = existsSync(join(browserDir, "index.original.html"))
+		? join(browserDir, "index.original.html")
+		: join(browserDir, "index.html");
 
-    const commonEngine = new CommonEngine();
+	const commonEngine = new CommonEngine();
 
-    server.set("view engine", "html");
-    server.set("views", browserDir);
-    server.set("trust proxy", true);
+	server.set("view engine", "html");
+	server.set("views", browserDir);
+	server.set("trust proxy", true);
 
-    server.disable("x-powered-by");
+	server.disable("x-powered-by");
 
-    const {
-        COOKIE_PARSER_SECRET_KEY,
-        CSRF_KEY
-    } = env.getValue();
+	const { COOKIE_PARSER_SECRET_KEY } = env.getValue();
 
-    // Middlewares
-    server.use((_, res, next) => {
-        res.set({
-            "Cache-Control": "public, max-age=604800, stale-while-revalidate=86400 immutable",
-            "Permissions-Policy": "camera=(), microphone=(), interest-cohort=()",
-            "Strict-Transport-Security": "max-age=31536000; includeSubDomains",
-            "X-Content-Type-Options": "nosniff",
-            "X-Frame-Options": "DENY",
-            "X-XSS-Protection": "1; mode=block"
-        });
-        next();
-    });
-    server.use(compression({
-        filter: (req, _,) => {
-            if (req.path.includes("/scan/images")) {
-                return false;
-            }
-            return true;
-        }
-    }));
-    server.use(json({
-        limit: 95 * 1024 * 1024
-    }));
-    server.use(cookieParser(COOKIE_PARSER_SECRET_KEY));
-    server.use(csurf({
-        cookie: {
-            sameSite: "strict",
-            httpOnly: true,
-            secure: CSRF_KEY.includes("__Host-"),
-            signed: CSRF_KEY.includes("__Host-"),
-            key: CSRF_KEY || "_CSRF"
-        },
-        value: req => String(req.headers["x-xftf-cre"])
-    }));
-    server.use((req, res, next) => {
-        res.cookie("X-Ftf-Token", req.csrfToken(), {
-            sameSite: "strict",
-            secure: CSRF_KEY.includes("__Host-")
-        });
-        next();
-    });
-    server.use(expressUserAgent());
-    server.use(mw());
-    server.use(exceptionToken);
+	// Middlewares
+	server.use((_, res, next) => {
+		res.set({
+			"Cache-Control": "public, max-age=604800, stale-while-revalidate=86400 immutable",
+			"Permissions-Policy": "camera=(), microphone=(), interest-cohort=()",
+			"Strict-Transport-Security": "max-age=31536000; includeSubDomains",
+			"X-Content-Type-Options": "nosniff",
+			"X-Frame-Options": "DENY",
+			"X-XSS-Protection": "1; mode=block"
+		});
+		next();
+	});
+	server.use(
+		compression({
+			filter: (req, _) => {
+				if (req.path.includes("/scan/images")) {
+					return false;
+				}
+				return true;
+			}
+		})
+	);
+	server.use(
+		json({
+			limit: 95 * 1024 * 1024
+		})
+	);
+	server.use(cookieParser(COOKIE_PARSER_SECRET_KEY));
+	server.use(expressUserAgent());
+	server.use(mw());
 
-    // Controller
-    server.get("/dataset", getAllDatasetController);
-    server.get("/dataset/:model/:name", getDatasetController);
-    server.patch("/dataset/:model/:name", [headerApi], replaceDatasetController);
-    server.post("/add/dataset", [headerApi], addDatasetController);
-    server.post("/download/dataset/:model/:name", [headerApi], downloadDatasetController);
-    server.delete("/dataset/:model/:name", [headerApi], removeDatasetController);
-    // 
-    server.get("/instruction", getInstructionStateController);
-    server.post("/save/instruction", [headerApi], saveInstructionStateController);
-    server.delete("/instruction", [headerApi], removeInstructionStateController);
-    // 
-    server.post("/scan/images", [headerApi, imageOCRUpload], imageOCRController);
+	// Controller
+	server.get("/dataset", getAllDatasetController);
+	server.get("/dataset/:model/:name", getDatasetController);
+	server.patch("/dataset/:model/:name", [headerApi], replaceDatasetController);
+	server.post("/add/dataset", [headerApi], addDatasetController);
+	server.post("/download/dataset/:model/:name", [headerApi], downloadDatasetController);
+	server.delete("/dataset/:model/:name", [headerApi], removeDatasetController);
+	//
+	server.get("/instruction", getInstructionStateController);
+	server.post("/save/instruction", [headerApi], saveInstructionStateController);
+	server.delete("/instruction", [headerApi], removeInstructionStateController);
+	//
+	server.post("/scan/images", [headerApi, imageOCRUpload], imageOCRController);
 
-    // Example Express Rest API endpoints
-    // server.get("/api/**", (req, res) => { });
-    // Serve static files from /browser
-    server.get("*.*", express.static(browserDir, {
-        maxAge: "1y"
-    }));
+	// Example Express Rest API endpoints
+	// server.get("/api/**", (req, res) => { });
+	// Serve static files from /browser
+	server.get(
+		"*.*",
+		express.static(browserDir, {
+			maxAge: "1y"
+		})
+	);
 
-    // All regular routes use the Angular engine
-    server.get("*", (req, res, next) => {
-        const { protocol, originalUrl, baseUrl, headers } = req;
-        commonEngine
-            .render({
-                bootstrap,
-                documentFilePath: indexHtml,
-                url: `${protocol}://${headers.host}${originalUrl}`,
-                publicPath: browserDir,
-                providers: [
-                    { provide: APP_BASE_HREF, useValue: baseUrl },
-                    { provide: RESPONSE, useValue: res },
-                    { provide: REQUEST, useValue: req }
-                ]
-            })
-            .then(html => res.send(html))
-            .catch(e => next(e));
-    });
+	// All regular routes use the Angular engine
+	server.get("*", (req, res, next) => {
+		const { protocol, originalUrl, baseUrl, headers } = req;
+		commonEngine
+			.render({
+				bootstrap,
+				documentFilePath: indexHtml,
+				url: `${protocol}://${headers.host}${originalUrl}`,
+				publicPath: browserDir,
+				providers: [
+					{ provide: APP_BASE_HREF, useValue: baseUrl },
+					{ provide: RESPONSE, useValue: res },
+					{ provide: REQUEST, useValue: req }
+				]
+			})
+			.then((html) => res.send(html))
+			.catch((e) => next(e));
+	});
 
-    return server;
+	return server;
 }
 
 function saveEnv() {
-    let envState: EnvVar | null = {};
-    const filepath = process.env["PATH_ENV_VAR"];
-    if (filepath) {
-        config({
-            processEnv: envState,
-            path: filepath
-        });
-    } else {
-        config({ processEnv: envState });
-    }
-    env.next(envState);
-    logInfo("Env var saved!");
-    // Cleanup
-    envState = null;
+	let envState: EnvVar | null = {};
+	const filepath = process.env["PATH_ENV_VAR"];
+	if (filepath) {
+		config({
+			processEnv: envState,
+			path: filepath
+		});
+	} else {
+		config({ processEnv: envState });
+	}
+	env.next(envState);
+	logInfo("Env var saved!");
+	// Cleanup
+	envState = null;
 }
 
 function run() {
-    const port = Math.abs(Number(process.env["PORT"])) || 12400;
-    const https = process.env["PROTOCOL_SERVER"] == "https";
-    if (https) {
-        const server = createServer({
-            cert: readFileSync(join(cwd(), "tls/fullchain.pem")),
-            key: readFileSync(join(cwd(), "tls/cert-key.pem")),
-            ca: readFileSync(join(cwd(), "tls/ca.crt"))
-        }, app());
-        server.listen(port, () => logInfo("Secure server listening on https://127.0.0.1:" + port));
-    } else {
-        const server = app();
-        server.listen(port, () => logInfo("Server listening on http://127.0.0.1:" + port));
-    }
+	const port = Math.abs(Number(process.env["PORT"])) || 12400;
+	const https = process.env["PROTOCOL_SERVER"] == "https";
+	if (https) {
+		const server = createServer(
+			{
+				cert: readFileSync(join(cwd(), "tls/fullchain.pem")),
+				key: readFileSync(join(cwd(), "tls/cert-key.pem")),
+				ca: readFileSync(join(cwd(), "tls/ca.crt"))
+			},
+			app()
+		);
+		server.listen(port, () => logInfo("Secure server listening on https://127.0.0.1:" + port));
+	} else {
+		const server = app();
+		server.listen(port, () => logInfo("Server listening on http://127.0.0.1:" + port));
+	}
 }
 
 saveEnv();
--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,8 +1,8 @@
-import { ApplicationConfig } from "@angular/core";
+import { ApplicationConfig, provideExperimentalZonelessChangeDetection } from "@angular/core";
 import { registerLocaleData } from "@angular/common";
 import { provideRouter, withEnabledBlockingInitialNavigation, withRouterConfig } from "@angular/router";
-import { provideHttpClient, withFetch, withXsrfConfiguration, withInterceptors } from "@angular/common/http";
-import { provideClientHydration } from "@angular/platform-browser";
+import { provideHttpClient, withFetch, withInterceptors } from "@angular/common/http";
+import { provideClientHydration, withEventReplay } from "@angular/platform-browser";
 import { provideAnimations } from "@angular/platform-browser/animations";
 
 import { routes } from "./app.routes";
@@ -18,35 +18,27 @@ import en from "@angular/common/locales/en";
 registerLocaleData(en);
 
 export const appConfig: ApplicationConfig = {
-    providers: [
-        provideRouter(
-            routes,
-            withRouterConfig({ resolveNavigationPromiseOnError: true }),
-            withEnabledBlockingInitialNavigation()
-        ),
-        provideClientHydration(),
-        provideHttpClient(
-            withFetch(),
-            withXsrfConfiguration({
-                cookieName: "X-Ftf-Token",
-                headerName: "X-Xftf-Cre"
-            }),
-            withInterceptors([
-                httpInterceptor
-            ])
-        ),
-        provideAnimations(),
-        provideNzI18n(en_US),
-        provideNzConfig({
-            notification: {
-                nzPauseOnHover: true,
-                nzPlacement: "top",
-                nzDuration: 5000,
-                nzMaxStack: 5
-            },
-            space: {
-                nzSize: 8
-            }
-        })
-    ]
+	providers: [
+		provideExperimentalZonelessChangeDetection(),
+		provideRouter(
+			routes,
+			withRouterConfig({ resolveNavigationPromiseOnError: true }),
+			withEnabledBlockingInitialNavigation()
+		),
+		provideClientHydration(withEventReplay()),
+		provideHttpClient(withFetch(), withInterceptors([httpInterceptor])),
+		provideAnimations(),
+		provideNzI18n(en_US),
+		provideNzConfig({
+			notification: {
+				nzPauseOnHover: true,
+				nzPlacement: "top",
+				nzDuration: 5000,
+				nzMaxStack: 5
+			},
+			space: {
+				nzSize: 8
+			}
+		})
+	]
 };
