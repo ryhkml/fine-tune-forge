397aea3155d3fe1211b38d002c7f6a5ec64612d2
ryhkml <ryhkmll@gmail.com>
2025-04-16 14:51:53 +0700

    Cleanup installation script and create update script

--- install.sh
+++ install.sh
@@ -1,19 +1,23 @@
+#!/usr/bin/env bash
+
 set -e
 
-cat <<EOF >.env
+tee .env > /dev/null <<'EOF'
 COOKIE_PARSER_SECRET_KEY=""
 # Optional
 ## For more information visit https://cloud.google.com/document-ai/docs/process-documents-client-libraries#client-libraries-usage-nodejs
 GCP_PROJECT_ID=""
 GCP_DOC_AI_PROCESSOR_CLIENT_EMAIL=""
 GCP_DOC_AI_PROCESSOR_PRIVATE_KEY=""
+# Processor name
+## projects/:id/locations/..
 GCP_DOC_AI_PROCESSOR_NAME=""
 ## 0 means false
 ## 1 means true
 GCP_DOC_AI_SKIP_HUMAN_REVIEW="0"
 EOF
 
-npm i --no-audit --no-fund
+npm ci --no-audit --no-fund
 
 echo
 echo "File .env created"
---
+++ update.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/env bash
+
+set -e
+
+npm ci --no-audit --no-fund
+
+echo "DONE!"
